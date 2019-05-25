240ca6717b1ba809abcd600570b79efa97cf6746
Giani Segatto <gsegatto@travix.com>
2019-05-25 16:39:08 +0100

    testing extension

--- src/main/node/datasource/Datasource.js
+++ src/main/node/datasource/Datasource.js
@@ -3,6 +3,8 @@ const ErrorBuilder = require("leap-core").ErrorBuilder;
 class Datasource {
 
     constructor(connectionPool, dataQuery) {
+        this.connectionPool = connectionPool;
+        this.dataQuery = dataQuery;
     }
 
     query(sql, values, rowMapper) {
